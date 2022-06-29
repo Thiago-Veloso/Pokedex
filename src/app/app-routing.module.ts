8e2c631b65816e02004a4092d3252e8bb404fb42
Thiago Pio <thiago.pio2017@gmail.com>
2022-06-29 08:44:14 -0300

    Created components for pages and routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { AddPokemonComponent } from './pages/add-pokemon/add-pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    component: MainComponent,
+  },
+  {
+    path: 'add',
+    component: AddPokemonComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'main',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { AddPokemonComponent } from './pages/add-pokemon/add-pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AddPokemonComponent
   ],
   imports: [
     BrowserModule,
